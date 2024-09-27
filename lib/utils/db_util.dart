6fe294d5cf9354303b56a00665f236f83db03fe1
Amilcar JB <amilcarjb@gmail.com>
2024-09-27 01:04:22 +0100

    Usando SQLite pra guardar os dados dos lugares em uma base de dados

---
+++ lib/utils/db_util.dart
@@ -0,0 +1,33 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DbUtil {
+  static Future<sql.Database> database() async {
+    final dbPath = await sql.getDatabasesPath();
+    return sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      //o onCreate é executado apenas na primeira vez que a bd rodar
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE places (id TEXT PRIMARY KEY, title TEXT, image TEXT)'
+            //Como já salvamos a imagem num diretorio local da app, na bd só salvamos já o diretorio
+            );
+      },
+      version: 1,
+    );
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DbUtil.database();
+    await db.insert(
+      table,
+      data,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DbUtil.database();
+    return db.query(table);
+  }
+}
